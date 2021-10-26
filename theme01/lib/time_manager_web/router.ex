bbc0d86544f26b67d20f88880fb5c5af2b201845
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-10-26 14:47:49 +0200

    fixed routes

--- theme01/lib/time_manager_web/router.ex
+++ theme01/lib/time_manager_web/router.ex
@@ -10,13 +10,13 @@ defmodule TimeManagerAPIWeb.Router do
 
     resources "/users", UserController, except: [:new, :edit]
 
-    scope "/workingtimes", WorkingTimeController do
+    scope "/workingtimes" do
       resources "/", WorkingTimeController, only: [:show, :update, :delete]
       get "/users/:userID", WorkingTimeController, :index
       post "/:userID", WorkingTimeController, :create
     end
 
-    scope "/clocks", ClockController do
+    scope "/clocks" do
       get "/:userID", ClockController, :index
       post "/:userID", ClockController, :update
     end
