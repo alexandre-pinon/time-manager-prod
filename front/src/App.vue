5d63ab8ba32506550663e328f912c50a7e059ed3
FroggEater <frogeater@outlook.fr>
2021-10-29 09:37:29 +0200

    Example chart

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,7 +2,12 @@
   <div id="app" class="application">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <User />
-    <ExampleChart chart-id="chart-example" type="bar" :data="chartData" :options="chartOptions" />
+    <ExampleChart
+      chart-id="chart-example"
+      type="bar"
+      :data="chartData"
+      :options="chartOptions"
+    />
   </div>
 </template>
 
@@ -24,23 +29,34 @@ export default Vue.extend({
   data() {
     return {
       chartData: {
-        labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+        labels: [
+          "Mercury",
+          "Venus",
+          "Earth",
+          "Mars",
+          "Jupiter",
+          "Saturn",
+          "Uranus",
+          "Neptune",
+        ],
         datasets: [
           {
             label: "Number of Moons",
             data: [0, 0, 1, 2, 79, 82, 27, 14],
             backgroundColor: "rgba(54,73,93,.5)",
             borderColor: "#36495d",
-            borderWidth: 3
+            borderWidth: 3,
           },
           {
             label: "Planetary Mass (relative to the Sun x 10^-6)",
-            data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
+            data: [
+              0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514,
+            ],
             backgroundColor: "rgba(71, 183,132,.5)",
             borderColor: "#47b784",
-            borderWidth: 3
-          }
-        ]
+            borderWidth: 3,
+          },
+        ],
       },
       chartOptions: {
         responsive: true,
@@ -50,11 +66,11 @@ export default Vue.extend({
             {
               ticks: {
                 beginAtZero: true,
-                padding: 25
-              }
-            }
-          ]
-        }
+                padding: 25,
+              },
+            },
+          ],
+        },
       },
     };
   },
