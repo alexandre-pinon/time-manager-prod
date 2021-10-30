253d0909c324fe427ce8405aaf0969d15ff53698
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-10-30 18:21:29 +0200

    added api dockerfile

---
+++ api/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Wait until Postgres is ready
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+# Create, migrate, and seed database if it doesn't exist.
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  mix ecto.setup
+  echo "Database $PGDATABASE created."
+else
+    echo "Database $PGDATABASE exists. Starting reset..."
+    mix ecto.reset
+    echo "Database $PGDATABASE reset."
+fi
+
+exec mix phx.server
