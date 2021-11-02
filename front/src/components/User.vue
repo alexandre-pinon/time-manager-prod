91b7b5afcf1247efd7c29f624644fca9b18d9a19
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-11-02 14:18:21 +0100

    moved style

--- front/src/components/User.vue
+++ front/src/components/User.vue
@@ -107,11 +107,6 @@ export default Vue.extend({
     CardForm,
     CardResult,
   },
-  filters: {
-    pretty: function (value: string) {
-      return JSON.stringify(value, null, 2);
-    },
-  },
   computed: {
     ...mapState(["currentUser"]),
   },
@@ -153,105 +148,4 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="scss">
-* {
-  box-sizing: border-box;
-}
-/* Remove extra left and right margins, due to padding in columns */
-
-.row {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  width: 100%;
-  margin: 20px 0;
-}
-
-.column {
-  display: flex;
-  flex-direction: row;
-  width: 25%;
-  padding: 0 16px;
-}
-
-@media screen and (max-width: 992px) {
-  .column {
-    width: 50%;
-    margin-bottom: 20px;
-  }
-}
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-  }
-}
-
-.card {
-  &-form,
-  &-result {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
-    padding: 16px;
-    text-align: center;
-    background-color: #f7f7f7;
-    transition: 0.3s;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-
-    &:hover {
-      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-    }
-  }
-
-  &-result {
-    margin: 0 16px;
-  }
-}
-
-.flexible {
-  flex-grow: 1;
-}
-
-input[type="text"],
-input[type="email"] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-
-button[type="button"] {
-  background-color: #4caf50;
-  color: white;
-  width: 100%;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #45a049;
-  }
-
-  &[disabled="disabled"] {
-    background-color: rgb(216, 216, 216);
-    color: grey;
-
-    &:hover {
-      background-color: rgb(216, 216, 216);
-    }
-  }
-}
-
-pre {
-  white-space: pre-wrap;
-  text-align: left;
-  font-size: large;
-  color: #2c3e50;
-  padding: 0 16px;
-}
-</style>
+<style lang="scss"></style>
--- front/src/components/forms/CardForm.vue
+++ front/src/components/forms/CardForm.vue
@@ -45,3 +45,66 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="scss">
+.card {
+  &-form,
+  &-result {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+    padding: 16px;
+    text-align: center;
+    background-color: #f7f7f7;
+    transition: 0.3s;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    &:hover {
+      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+  }
+
+  &-result {
+    margin: 0 16px;
+  }
+}
+
+.flexible {
+  flex-grow: 1;
+}
+
+input[type="text"],
+input[type="email"] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+button[type="button"] {
+  background-color: #4caf50;
+  color: white;
+  width: 100%;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #45a049;
+  }
+
+  &[disabled="disabled"] {
+    background-color: rgb(216, 216, 216);
+    color: grey;
+
+    &:hover {
+      background-color: rgb(216, 216, 216);
+    }
+  }
+}
+</style>
