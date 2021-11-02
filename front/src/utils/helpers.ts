82f000f4420b3daf68dcf09958a9421dd7908744
FroggEater <frogeater@outlook.fr>
2021-11-02 17:01:56 +0100

    Newline fix

--- front/src/utils/helpers.ts
+++ front/src/utils/helpers.ts
@@ -12,4 +12,4 @@ export const hexToRGB = (hex: string, alpha: string): string => {
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5, 7), 16);
   return alpha ? `rgba(${r}, ${g}, ${b}, ${alpha})` : `rgb(${r}, ${g}, ${b})`;
-}
+};
