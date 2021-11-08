941b0665ae2aaaa119b8016ae2d3319db2448cca
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-11-08 15:29:38 +0100

    fix dockerfiles

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 
+echo 'Replacing env vars...'
 for file in $JSFOLDER;
 do
-  cat $file | envsubst $EXISTING_VARS | tee $file
+  cat $file | envsubst $EXISTING_VARS > $file
 done
+echo 'DONE'
 
 exec nginx -g 'daemon off;'
