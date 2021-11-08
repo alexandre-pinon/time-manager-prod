278fddb35f47b0752b08ed133224aa3f5be9fda3
Encarnacao <leandro.encarnacao@epitech.eu>
2021-11-08 12:38:05 +0100

    mobile error fix cardResult

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   lintOnSave: false,
+
   css: {
     loaderOptions: {
       sass: {
         additionalData: `@import "~@/scss/_variables.scss";`,
       },
     },
+  },
+
+  publicPath: "",
+
+  pluginOptions: {
+    cordovaPath: "timeManager",
   },
 };
