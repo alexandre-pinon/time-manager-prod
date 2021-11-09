6b762766ed063f52f19c8f58f0f51df97f05de42
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-11-09 16:23:45 +0100

    added user_id & role in login/register response

--- api/lib/time_manager_web/controllers/user_registration.ex
+++ api/lib/time_manager_web/controllers/user_registration.ex
@@ -2,20 +2,24 @@ defmodule TimeManagerAPIWeb.UserRegistration do
   use TimeManagerAPIWeb, :controller
 
   alias Ecto.Changeset
-  alias Plug.Conn
   alias TimeManagerAPIWeb.ErrorHelpers
-  alias TimeManagerAPI.Auth.UserRegistration
 
   def sign_up(conn, %{"user" => user_registration_command}) do
     with {:ok, _user, conn} <- conn |> Pow.Plug.create_user(user_registration_command) do
-      json(conn, %{token: conn.private[:api_access_token]})
+      json(conn, %{
+        data: %{
+          id: conn.private.user_id,
+          role: conn.private.role,
+          token: conn.private.api_access_token
+        }
+      })
     else
       {:error, changeset, conn} ->
         errors = Changeset.traverse_errors(changeset, &ErrorHelpers.translate_error/1)
 
         conn
-        |> put_status(500)
-        |> json(%{error: %{status: 500, message: "Couldn't create user", errors: errors}})
+        |> put_status(:bad_request)
+        |> json(%{errors: errors})
     end
   end
 end
