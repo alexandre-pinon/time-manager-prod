c3a25453d4e308a83c4ebdea73cc2a71047e69fe
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-11-12 14:14:36 +0100

    fix front deployment

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -4,7 +4,7 @@ export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste 
 echo 'Replacing env vars...'
 for file in $JSFOLDER;
 do
-  cat $file | envsubst $EXISTING_VARS > $file
+  cat $file | envsubst $EXISTING_VARS | tee $file
 done
 echo 'DONE'
 
